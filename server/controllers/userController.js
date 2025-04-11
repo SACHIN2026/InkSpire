e9cbad2ea223f1a18ae48c0567ff59ef110dd540
Sachin <sachsaini2004@gmail.com>
2025-04-12 01:16:27 +0530

    feat: simplify getProfile function to return user data directly from request

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,10 +1,15 @@
-import User from "../models/User.js";
+// import User from "../models/User.js";
 
-export const getProfile = async (req, res) =>{
-    try {
-        const user = await User.findById(req.user.id).select('-password');
-        res.status(200).json(user);
-    } catch (error) {
-        res.status(500).json({message : error.message});
-    }
-}
+// export const getProfile = async (req, res) =>{
+//     try {
+//         const user = await User.findById(req.user.id).select('-password');
+//         res.status(200).json(user);
+//     } catch (error) {
+//         res.status(500).json({message : error.message});
+//     }
+// }
+
+export const getProfile = (req, res) => {
+    res.status(200).json(req.user);
+  };
+  

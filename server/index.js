eca120b10e045cdb45aabdf070a32e06cb5b6d8d
Sachin <sachsaini2004@gmail.com>
2025-04-11 23:55:21 +0530

    feat: restructure index.js to properly import and use auth and user routes

--- server/index.js
+++ server/index.js
@@ -2,19 +2,25 @@ import express from 'express';
 import cors from 'cors';
 import connectDB from './config/db.js';
 import dotenv from 'dotenv';
+import authRoutes from './routes/authRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
+
+dotenv.config();
+connectDB()
 const app = express();
 const PORT = process.env.PORT || 5000;
-dotenv.config();
 cors(app);
 
+app.use(cors());
 app.use(express.json());
+app.use('/api/auth', authRoutes);
+app.use('/api/users', userRoutes);
 
 app.get('/', (req, res) =>{
     res.send("Hello from server")
 })
 
-connectDB()
 
 app.listen(PORT, () =>{
     console.log("Server is running on port ", PORT);

cc256e48c0c3096414cfd8f1337188b7ccad18e9
Sachin <sachsaini2004@gmail.com>
2025-04-13 02:22:33 +0530

    feat: enhance tailwind configuration with custom colors and daisyui themes

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,35 @@
-// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#0B1C2C",   // Deep navy blue
+        secondary: "#F7F3EC", // Soft beige/cream
+      },
+    },
   },
-  plugins: [],
-};
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#0B1C2C",
+          "secondary": "#F7F3EC",
+          "accent": "#dcca87",
+          "neutral": "#3D4451",
+          "base-100": "#FFFFFF",
+          "info": "#3ABFF8",
+          "success": "#36D399",
+          "warning": "#FBBD23",
+          "error": "#F87272",
+        },
+      },
+      "dark",
+      "light",
+    ],
+  },
+}

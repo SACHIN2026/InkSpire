bd8c2bcab2c1efb6cd2ee6bec8216a41408fd1e0
Sachin <sachsaini2004@gmail.com>
2025-05-27 23:40:18 +0530

    feat: update axios instance to use environment variable for base URL

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  // baseURL: import.meta.env.VITE_URL, // Your backend API URL
-  baseURL: 'http://localhost:5000/api', // Your backend API URL
+  baseURL: import.meta.env.VITE_URL, // Your backend API URL
+  // baseURL: 'http://localhost:5000/api', // Your backend API URL
   headers: {
     'Content-Type': 'application/json'
   }
